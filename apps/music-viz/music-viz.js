ec5c7f6e588d0245912458e592a210f9830c225d
mmalviyarfl <78374110+mmalviyarFL@users.noreply.github.com>
2024-10-13 12:27:41 +0200

    updating resume

--- apps/music-viz/music-viz.js
+++ apps/music-viz/music-viz.js
@@ -143,6 +143,7 @@ class MusicApp {
             } else {
                 this.elements.appWindow.classList.remove('full-screen-modal');
                 this.elements.toggleFullScreen.innerHTML = '<i class="bi bi-arrows-fullscreen"></i>'; // Change icon when exiting full-screen
+                this.hideMusicHud(); // Hide the music HUD when exiting full-screen
             }
         });
     }
