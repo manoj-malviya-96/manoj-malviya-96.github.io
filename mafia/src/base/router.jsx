8ec5ac57e4a8d8457fb1b6e8b0e1f7ef2e037592
manoj-malviya-96 <manojmalviya@raiden.local>
2024-11-24 23:15:06 +0100

    animation

--- mafia/src/base/router.jsx
+++ mafia/src/base/router.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
-import { AnimatePresence } from "framer-motion";
+import { AnimatePresence } from "motion/react";
 import SlidingAnimation from "./sliding-animation";
 import { useEffect } from "react";
 
--- mafia/src/base/sliding-animation.jsx
+++ mafia/src/base/sliding-animation.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { motion } from "framer-motion";
+import { motion } from "motion/react"
 
 const slidingVariants = {
     initial: { y: "100vh", opacity: 0 },
