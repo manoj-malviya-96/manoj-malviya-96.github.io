69677d74ddffff6f12bd34d735262674cf52b5ec
manoj-malviya-96 <manojmalviya@raiden.local>
2024-11-25 18:37:21 +0100

    rearrange

--- mafia/src/main/home.jsx
+++ mafia/src/main/home.jsx
@@ -9,16 +9,16 @@ import {createTabItem} from "../utils/types";
 const Home = () => {
     const tabs = [
         createTabItem({name: 'intro', label: 'Intro', icon: 'fas fa-info-circle'}),
-        createTabItem({name: 'career', label: 'Career', icon: 'fas fa-briefcase'}),
         createTabItem({name: 'tools', label: 'Tools', icon: 'fas fa-tools'}),
         createTabItem({name: 'blog', label: 'Blog', icon: 'fas fa-blog'}),
+        createTabItem({name: 'career', label: 'About Me', icon: 'fas fa-person'}),
     ]
     return (
         <div className='flex-row w-fit h-fit'>
             <Intro/>
-            <Career/>
             <Tools/>
             <BlogListing/>
+            <Career/>
             {/*match the top margin of the navbar*/}
             <div className='flex-column bg-base-100 bg-opacity-50 backdrop-blur-md m-4
                             rounded-2xl fixed top-0 w-fit h-fit z-20'>
