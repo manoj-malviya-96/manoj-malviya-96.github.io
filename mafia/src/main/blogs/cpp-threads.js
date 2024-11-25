089cff74b36e885b374a49bf3f8d5fa4617c1e97
manoj-malviya-96 <manojmalviya@macbookair.localdomain>
2024-11-26 00:05:23 +0100

    rearrange

--- mafia/src/main/blogs/cpp-threads.js
+++ mafia/src/main/blogs/cpp-threads.js
@@ -2,59 +2,57 @@ import {BlogInfo, makeBlogCode, makeBlogSectionContent} from "./blog-info";
 import Cover from "./cpp-threads.webp"
 import {SizeOptions} from "../../utils/enums";
 
-
-const simpleThreadsExample = makeBlogSectionContent(
-    {
-        name: 'example',
-        icon: 'fa fa-info-circle',
-        title: 'example',
-        paragraph: (`<p>When I first started exploring C++ threads, it was like stepping into an entirely new territory. C++ has
+class CppThreads extends BlogInfo {
+    constructor() {
+        const simpleThreadsExample = makeBlogSectionContent(
+            {
+                name: 'example',
+                icon: 'fa fa-info-circle',
+                title: 'example',
+                paragraph: (`<p>When I first started exploring C++ threads, it was like stepping into an entirely new territory. C++ has
                 built-in support for multi-threading via the <code>&lt;thread&gt;</code> library.
                 The basic idea is that
                 a thread runs a separate flow of execution, allowing you to do multiple things at once. At first, I
                 thought, “How hard can this be?”—but as I quickly found out, threads can get pretty wild if you don't
                 manage them carefully.</p>`),
-        media: makeBlogCode({
-            language: 'cpp', code: '#include iostream;\n' +
-                '#include thread;\n' +
-                '\n' +
-                'void print_message() {\n' +
-                '    std::cout: "Hello from a thread!" &lt;&lt; std::endl;\n' +
-                '}\n' +
-                '\n' +
-                'int main() {\n' +
-                '    std::thread t(print_message); // Create a thread to run print_message\n' +
-                '    t.join(); // Wait for the thread to finish\n' +
-                '    return 0;'
-        })
-    });
+                media: makeBlogCode({
+                    language: 'cpp', code: '#include iostream;\n' +
+                        '#include thread;\n' +
+                        '\n' +
+                        'void print_message() {\n' +
+                        '    std::cout: "Hello from a thread!" &lt;&lt; std::endl;\n' +
+                        '}\n' +
+                        '\n' +
+                        'int main() {\n' +
+                        '    std::thread t(print_message); // Create a thread to run print_message\n' +
+                        '    t.join(); // Wait for the thread to finish\n' +
+                        '    return 0;'
+                })
+            });
 
-const raceConditions = makeBlogSectionContent({
-    name: 'race-conditions',
-    icon: 'fa fa-person-running',
-    title: 'race-conditions',
-    paragraph: (`<p>One of the first things I ran into was a <strong>race condition</strong>. It’s like having multiple
+        const raceConditions = makeBlogSectionContent({
+            name: 'race-conditions',
+            icon: 'fa fa-person-running',
+            title: 'race-conditions',
+            paragraph: (`<p>One of the first things I ran into was a <strong>race condition</strong>. It’s like having multiple
                 people trying to update the same Excel file simultaneously—everyone thinks they're editing the latest
                 version, but chaos ensues. Let me paint a picture: I had a shared <strong>inventory map</strong> that
                 stored the stock count of different items. Imagine multiple threads trying to update this map at the same time. It might look something like
                 this:</p>`),
-    media: makeBlogCode({
-        language: 'cpp', code: '#include iostream;\n' +
-            '#include thread;\n' +
-            '\n' +
-            'void print_message() {\n' +
-            '    std::cout: "Hello from a thread!" &lt;&lt; std::endl;\n' +
-            '}\n' +
-            '\n' +
-            'int main() {\n' +
-            '    std::thread t(print_message); // Create a thread to run print_message\n' +
-            '    t.join(); // Wait for the thread to finish\n' +
-            '    return 0;'
-    })
-});
-
-class CppThreads extends BlogInfo {
-    constructor() {
+            media: makeBlogCode({
+                language: 'cpp', code: '#include iostream;\n' +
+                    '#include thread;\n' +
+                    '\n' +
+                    'void print_message() {\n' +
+                    '    std::cout: "Hello from a thread!" &lt;&lt; std::endl;\n' +
+                    '}\n' +
+                    '\n' +
+                    'int main() {\n' +
+                    '    std::thread t(print_message); // Create a thread to run print_message\n' +
+                    '    t.join(); // Wait for the thread to finish\n' +
+                    '    return 0;'
+            })
+        });
         super({
             id: 'cpp-threads',
             title: 'Understanding Multi-Threading',
