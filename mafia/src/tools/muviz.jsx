1bf0727d04850f6dfc703f17eb1eec5d0954cee1
manoj-malviya-96 <manojmalviya@raiden.local>
2024-11-25 18:12:52 +0100

    animation

--- mafia/src/tools/muviz.jsx
+++ mafia/src/tools/muviz.jsx
@@ -9,8 +9,6 @@ const MuvizView = () => {
             title="Muviz"
             children={
                 <div className="flex flex-col items-center justify-center h-full">
-                    <h1 className="text-4xl font-bold">Muviz</h1>
-                    <p className="text-lg mt-4">Coming soon...</p>
                 </div>
             }
         />
