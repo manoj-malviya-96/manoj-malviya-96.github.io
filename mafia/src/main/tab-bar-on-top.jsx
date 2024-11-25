211c6be4938d925f039f4a9ab86a3ef501800f5f
manoj-malviya-96 <manojmalviya@macbookair.localdomain>
2024-11-26 00:40:13 +0100

    rearrange

--- mafia/src/main/tab-bar-on-top.jsx
+++ mafia/src/main/tab-bar-on-top.jsx
@@ -3,7 +3,6 @@ import React from "react";
 
 
 const TabBarOnTop = ({tabs}) => {
-    console.log(tabs);
     return (<div className='flex-column p-4
                             rounded-2xl fixed top-0 w-fit h-fit z-20'>
         <TabBar tabs={tabs} className='bg-opacity-75 bg-primary rounded-full backdrop-blur-lg'/>
