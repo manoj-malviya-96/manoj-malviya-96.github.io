888057765ba86f38bfeaa413e1327b9303f78e9b
manoj-malviya-96 <manojmalviya@macbookair.localdomain>
2024-11-26 14:34:55 +0100

    rearrange

--- mafia/src/base/primary-button.jsx
+++ mafia/src/base/primary-button.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import { motion } from "motion/react"
+import {motion} from "motion/react"
 import {ButtonOptions} from "../utils/enums";
 
 const PrimaryButton = ({
@@ -12,28 +12,27 @@ const PrimaryButton = ({
                            state = ButtonOptions.State.None,
                            style = ButtonOptions.Style.Filled,
                            behavior = ButtonOptions.Behavior.Default,
+                           roundness = ButtonOptions.Round.Full,
                            isLoading = false,
                            className = '',
                        }) => {
-    const padAndGap= size !== ButtonOptions.Size.Square ? 'px-4 py-1 gap-2' : ''
-    const isIconSVG = icon && icon.endsWith('.svg');
+
+    const padAndGap = size !== ButtonOptions.Size.Square ? 'px-4 py-1 gap-2' : ''
+
     return (
         <motion.button
-            className={`btn ${size} ${state} ${style} ${behavior} rounded-full items-center ${padAndGap} ${className}`}
+            className={`btn ${size} ${state} ${style} ${behavior} ${roundness} items-center ${padAndGap} ${className}`}
             onClick={onClick}
             disabled={isLoading || state === ButtonOptions.State.Disabled} // Disable while loading
             aria-label={label || 'Button'}
-            whileHover={{ scale: 1.1 }}
-            whileTap={{ scale: 0.9 }}
+            whileHover={{scale: 1.1}}
+            whileTap={{scale: 0.9}}
         >
             {/* Loading Spinner */}
             {isLoading && <span className="loading loading-spinner"></span>}
 
             {/* Icon */}
-            {icon && !isLoading && isIconSVG && <img src={icon} alt="icon"></img>}
-
-            {/* Icon */}
-            {icon && !isLoading && !isIconSVG && <i className={icon}></i>}
+            {icon && !isLoading && <i className={icon}></i>}
 
             {/* Label */}
             {label && !isLoading && (
