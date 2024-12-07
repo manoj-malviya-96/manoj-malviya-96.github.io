cd8873231be82ee5856e921d3f561a8ee8ac7834
manoj-malviya-96 <manojmalviya@raiden.local>
2024-12-07 11:35:37 +0100

    move

--- mafia/src/atoms/scroll-container.jsx
+++
@@ -1,9 +0,0 @@
-const ScrollContainer = ({className, children}) => {
-    return (
-        <div className={`w-full h-full ${className}
-            overflow-scroll scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-100`}
-             >
-            {children}
-        </div>);
-};
-export default ScrollContainer;
