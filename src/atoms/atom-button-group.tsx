6c668e29213fa9b6b9176644fb74f67706272659
Manoj Malviya <malviyamanoj1896@gmail.com>
2024-12-09 20:58:43 +0100

    button group optimized

--- src/atoms/atom-button-group.tsx
+++ src/atoms/atom-button-group.tsx
@@ -10,18 +10,16 @@ interface AtomButtonGroupProps {
 
 const AtomButtonGroup: React.FC<AtomButtonGroupProps> = React.memo(({items}) => {
     return (
-        <div className="justify-content-center w-fit h-fit bg-black bg-opacity-25 rounded-lg">
-            <ButtonGroup>
-                {items.map((item, index) => (
-                    <AtomButton
-                        key={index}
-                        {...item}
-                        ghost={true}
-                        rounded={false}
-                        size="large"
-                    />
-                ))}
-            </ButtonGroup>
+        <div className="flex flex-row gap-0 justify-content-center w-fit h-fit bg-black bg-opacity-25 rounded-lg">
+            {items.map((item, index) => (
+                <AtomButton
+                    key={index}
+                    {...item}
+                    ghost={true}
+                    rounded={false}
+                    size="large"
+                />
+            ))}
         </div>
     );
 });
