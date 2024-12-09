1d1a6fca74e00b3d618e79bb82f23f8b06ef21d1
Manoj Malviya <malviyamanoj1896@gmail.com>
2024-12-09 23:30:13 +0100

    stalin

--- src/main/intro.tsx
+++ src/main/intro.tsx
@@ -1,51 +1,6 @@
 import React from 'react';
 import FullScreenPage from "../atoms/full-page";
-import AtomButtonGroup from "../atoms/atom-button-group";
-import {rangesTo} from "../common/types";
-import {openLink} from "../common/links";
-import {AtomButtonProps} from "../atoms/atom-button";
 import ProfilePicture from "./assets/main.jpg";
-
-
-interface IntroOverlayProps {
-    socialMediaItems: AtomButtonProps[];
-}
-
-const IntroOverlay: React.FC<IntroOverlayProps> = ({socialMediaItems}) => {
-    return (
-        <div className="absolute left-8 top-1/3 w-1/3">
-            <p className="text-lg mb-1 text-black">
-                Manoj Malviya
-            </p>
-            <h1 className="text-4xl text-indigo-600 font-bold mb-4 uppercase">
-                Crafting innovation with MAFIA—Manoj's Artistic
-                Fusion for Interactive Applications. Where art meets technology,
-                and creativity drives solutions."
-            </h1>
-            <AtomButtonGroup items={socialMediaItems}/>
-        </div>
-    )
-}
-
-type SocialMediaLink = [icon: string, link: string, tooltip: string];
-const MySocialMediaLinks: Array<SocialMediaLink> = [
-    ['fa-brands fa-linkedin', 'https://www.linkedin.com/in/manoj-malviya-44700aa4/', 'linkedin'],
-    ['fa-brands fa-github', 'https://github.com/manoj-malviya-96', 'github'],
-    ['fa-brands fa-google', 'https://scholar.google.com/citations?user=0oMXOy0AAAAJ&hl=en&authuser=2',
-        'google scholar'],
-    ['fa-brands fa-instagram', 'https://www.instagram.com/manoj_malviya_/', 'instagram'],
-    ['fa-brands fa-youtube', 'https://www.youtube.com/@manoj_malviya', 'youtube'],
-    ['fa-brands fa-apple', 'https://music.apple.com/us/artist/manoj-malviya/1721435458', 'apple music'],
-    ['fa-brands fa-soundcloud', 'https://soundcloud.com/manoj-malviya-96', 'soundcloud'],
-];
-const socialMediaItems = rangesTo(
-    MySocialMediaLinks, (smLink: SocialMediaLink) => {
-        return {
-            icon: smLink[0],
-            onClick: () => openLink(smLink[1], null),
-            tooltip: smLink[2],
-        } as AtomButtonProps;
-    });
 
 const Intro = () => {
     return (
@@ -53,7 +8,16 @@ const Intro = () => {
             name="intro"
             backgroundImage={ProfilePicture}
             children={
-                <IntroOverlay socialMediaItems={socialMediaItems}/>
+                (
+                    <div className="absolute left-8 top-1/3 w-fit md:w-1/3 p-4">
+                        <p className="text-lg mb-1 text-black">
+                            Manoj's
+                        </p>
+                        <h1 className="text-4xl text-black font-bold mb-4 uppercase">
+                            Audacious Framework for Interactive Apps
+                        </h1>
+                    </div>
+                )
             }
         />
     );
--- src/main/navbar.tsx
+++ src/main/navbar.tsx
@@ -43,7 +43,8 @@ const Navbar = () => {
             className={'z-20'}
             start={(
                 <div className='flex flex-row w-fit h-fit justify-center items-center
-                        gap-2 px-4 py-2 m-0 bg-primary bg-opacity-50 rounded-full '>
+                        backdrop-blur-lg gap-2 px-4 py-2 m-0
+                        bg-primary bg-opacity-50 rounded-full '>
                     <img src={logo} alt={name} className='w-6 h-6'/>
                     <h1 className='text-lg font-bold text-center m-auto'>{name}</h1>
                 </div>
