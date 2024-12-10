5ac5e7f3c402df60ef6ee0149fd25bc2f9377cc1
Manoj Malviya <malviyamanoj1896@gmail.com>
2024-12-10 21:36:03 +0100

    adds muviz

---
+++ src/main/tools/muviz/muviz.tsx
@@ -0,0 +1,355 @@
+import React, {useCallback, useRef, useState} from 'react';
+import ToolInfo from "../tool-info";
+import {
+    AudioPlayerProps,
+    useAudioPlayer
+} from "../../../common/audio";
+import {AtomButton} from "../../../atoms/atom-button";
+import Logo from '../logos/muviz.svg';
+import AtomDropdown, {
+    AtomDropdownItemProps
+} from "../../../atoms/atom-dropdown";
+
+import CallingON from './sample-music/calling.mp3';
+import CanYouFeelIt from './sample-music/can_u_feel_it.mp3';
+import Uneath from './sample-music/uneath.mp3';
+
+import {Canvas, CanvasController} from "../../../atoms/canvas";
+import AtomSlider from "../../../atoms/atom-slider";
+import {formatTime} from "../../../common/date";
+import {
+    BarVisualizer,
+    SpiralVisualizer,
+    toString,
+    VisualizerType
+} from "./visualizers";
+import AtomFileUpload from "../../../atoms/atom-file-upload";
+import ModalButton from "../../../atoms/modal-button";
+import {toggleFullScreen} from "../../../common/full-screen";
+import AppView from "../../../atoms/app-view";
+
+const AppName = 'MUVIZ';
+
+interface MuvizAppProps {
+    songOptions: AtomDropdownItemProps[];
+    vizOptions: AtomDropdownItemProps[];
+}
+
+const MuvizApp: React.FC<MuvizAppProps> = ({
+                                               songOptions,
+                                               vizOptions
+                                           }) => {
+    const timeSkip_s = 10;
+    
+    // State Management
+    const [src, setSrc] = useState<AudioPlayerProps["src"]>(null);
+    const [visualizerType, setVisualizerType] = useState(VisualizerType.Spiral);
+    const [controller, setController] = useState<CanvasController | null>(null);
+    const [isFullScreen, setIsFullScreen] = useState<boolean>(false);
+    const appRef = useRef<HTMLDivElement | null>(null);
+    
+    // Player Setup
+    const {
+        analyser,
+        dataArray,
+        isPlaying,
+        play,
+        pause,
+        currentTime,
+        setAudioTime,
+        volume,
+        changeVolume,
+        title,
+        duration,
+    } = useAudioPlayer({src, makeAnalyzer: true});
+    
+    const updateVisualizer = useCallback(async () => {
+        if (!analyser || !dataArray) {
+            return;
+        }
+        
+        switch (visualizerType) {
+            case VisualizerType.Spiral:
+                const spiralViz = new SpiralVisualizer({
+                    analyser: analyser,
+                    dataArray: dataArray,
+                });
+                setController(spiralViz);
+                break;
+            case VisualizerType.Bar:
+                const barViz = new BarVisualizer({
+                    analyser: analyser,
+                    dataArray: dataArray,
+                });
+                setController(barViz);
+                break;
+            default:
+                throw new Error("Invalid visualizer type");
+        }
+    }, [analyser, dataArray, visualizerType]);
+    
+    const stopController = useCallback(() => {
+        if (controller) {
+            controller.stop();
+        }
+    }, [controller]);
+    
+    const handleSampleSongChange = useCallback(
+        (value: any) => {
+            setSrc(value);
+            stopController();
+            pause();
+        },
+        [stopController, pause]
+    );
+    
+    const handleVisualizerChange = useCallback(
+        (value: any) => {
+            setVisualizerType(value);
+            stopController();
+            pause();
+        },
+        [stopController, pause]
+    );
+    
+    const handlePlayOrPause = useCallback(() => {
+        if (isPlaying) {
+            stopController();
+            pause();
+        } else {
+            updateVisualizer().then(() => play());
+        }
+    }, [play, pause, stopController, updateVisualizer, isPlaying]);
+    
+    const skipForward = useCallback(() => {
+        setAudioTime(currentTime + timeSkip_s);
+    }, [currentTime, setAudioTime]);
+    
+    const skipBackward = useCallback(() => {
+        setAudioTime(currentTime - timeSkip_s);
+    }, [currentTime, setAudioTime]);
+    
+    const handleToggleFullScreen = useCallback(() => {
+        toggleFullScreen(appRef.current, isFullScreen).then(() => setIsFullScreen(!isFullScreen));
+    }, [isFullScreen]);
+    
+    const handleFileChange = useCallback((file: string) => {
+        if (file) {
+            setSrc(file);
+        }
+    }, []);
+    
+    const toggleVolume = useCallback(() => {
+        changeVolume(volume === 0 ? 0.69 : 0);
+    }, [volume, changeVolume]);
+    const showHUD = !isPlaying;
+    
+    
+    // Render
+    return (
+        <div className="h-full w-full justify-center align-center"
+             ref={appRef}>
+            <Canvas controller={controller}
+                    className="absolute top-0 left-0 w-full h-full z-0"/>
+            
+            {/*HUD*/}
+            <div className={`inline-block w-full h-full z-5 p-4
+                ${showHUD ? "lg:opacity-100" : "lg:opacity-0"}
+                 lg:hover:opacity-100`}>
+                {/*Central Controls*/}
+                <div
+                    className="flex flex-wrap sm:flex-nowrap w-full h-fit justify-center
+                        items-center gap-4 absolute left-1/2 top-1/2 transform -translate-x-1/2">
+                    <AtomButton
+                        icon="fa-solid fa-arrow-rotate-left"
+                        neutralGhost={true}
+                        size="large"
+                        onClick={skipBackward}
+                        disabled={!src || currentTime < 10}
+                    />
+                    <AtomButton
+                        icon={isPlaying ? "fa fa-pause" : "fa fa-play"}
+                        disabled={!src}
+                        size="large"
+                        neutralGhost={true}
+                        onClick={handlePlayOrPause}
+                    />
+                    <AtomButton
+                        icon="fa-solid fa-arrow-rotate-right"
+                        size="large"
+                        onClick={skipForward}
+                        disabled={!src}
+                        neutralGhost={true}
+                    />
+                </div>
+                
+                {/*Bottom HUD*/}
+                <div
+                    className="flex flex-col gap-4 w-full rounded-lg h-fit absolute left-0 bottom-0 p-4">
+                    
+                    {/* Metadata */}
+                    <div
+                        className="flex flex-wrap sm:flex-nowrap justify-between items-center w-full h-full gap-4">
+                        <span
+                            className="text-base text-white sm:text-lg font-bold">
+                            {title ? title : "No Song"}
+                        </span>
+                        <span
+                            className="text-xs text-white sm:text-sm">
+                            {formatTime(currentTime)} / {formatTime(duration)}
+                        </span>
+                    </div>
+                    
+                    {/* Time Slider*/}
+                    <AtomSlider
+                        value={currentTime}
+                        min={0}
+                        max={duration || 0}
+                        step={0.1}
+                        onChange={setAudioTime}
+                        className="w-full h-fit"
+                        neutralMode={true}
+                    />
+                    
+                    
+                    <div
+                        className="flex flex-wrap sm:flex-nowrap justify-between
+                                    items-center w-full h-fit"
+                    >
+                        {/* Volume Row */}
+                        <div
+                            className="w-48 flex flex-row gap-1 items-center">
+                            <AtomButton
+                                icon={
+                                    volume === 0
+                                    ? "fa-solid fa-volume-xmark"
+                                    : volume > 0.69
+                                      ? "fa-solid fa-volume-high"
+                                      : "fa-solid fa-volume-low"
+                                }
+                                neutralGhost={true}
+                                onClick={toggleVolume}
+                            />
+                            <AtomSlider
+                                value={volume}
+                                min={0}
+                                max={1}
+                                step={0.01}
+                                onChange={changeVolume}
+                                neutralMode={true}
+                                size="small"
+                                className={"w-1/3 h-fit"}
+                            />
+                        </div>
+                        
+                        {/* Right Controls*/}
+                        <div
+                            className="w-full lg:w-fit h-full flex flex-row gap-4 justify-around">
+                            <AtomDropdown
+                                options={songOptions}
+                                dropdownIcon={"fas fa-music"}
+                                onClick={handleSampleSongChange}
+                                className="h-full w-fit m-auto"
+                                placeholder="Select Song"
+                                neutralMode={true}
+                            />
+                            <AtomDropdown
+                                options={vizOptions}
+                                onClick={handleVisualizerChange}
+                                className="h-full w-fit m-auto"
+                                placeholder="Select Visualizer"
+                                initialIndex={0}
+                                neutralMode={true}
+                            />
+                            <ModalButton
+                                icon="fa-solid fa-plus"
+                                title="Choose Music"
+                                className="h-full w-fit m-auto"
+                                onClick={() => {
+                                }}
+                                dialogContent={
+                                    <div
+                                        className="flex flex-col gap-2">
+                                        <span className="text-base">Upload Music</span>
+                                        <span className="text-xs">Only audio files are supported</span>
+                                        <AtomFileUpload
+                                            acceptTypes="audio/*"
+                                            onFileChange={handleFileChange}/>
+                                    </div>
+                                }
+                                neutralGhost={true}
+                                addOkButton={true}
+                            />
+                            <AtomButton
+                                icon={isFullScreen ? "fa fa-compress" : "fa fa-expand"}
+                                className="h-full w-fit m-auto"
+                                onClick={handleToggleFullScreen}
+                                neutralGhost={true}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+const defaultSongOptions =
+    [
+        {
+            label: 'calling on',
+            value: CallingON,
+        } as AtomDropdownItemProps,
+        {
+            label: 'can you feel it',
+            value: CanYouFeelIt,
+        } as AtomDropdownItemProps,
+        {
+            label: 'underneath it all',
+            value: Uneath,
+        } as AtomDropdownItemProps
+    ];
+
+
+const defaultVizOptions = [
+    {
+        label: toString(VisualizerType.Bar),
+        value: VisualizerType.Bar,
+    } as AtomDropdownItemProps,
+    {
+        label: toString(VisualizerType.Spiral),
+        value: VisualizerType.Spiral,
+    } as AtomDropdownItemProps
+]
+
+const MuvizView = () => {
+    return (
+        <AppView
+            appName={AppName}
+            appLogo={Logo}
+            children={
+                <MuvizApp songOptions={defaultSongOptions}
+                          vizOptions={defaultVizOptions}/>
+            }
+        />
+    )
+}
+
+class Muviz extends ToolInfo {
+    constructor() {
+        super({
+            id: 'muviz',
+            name: AppName,
+            description: 'music + stunning visuals',
+            cover: Logo,
+            componentConstructor: () => (
+                <MuvizView/>
+            )
+        });
+    }
+}
+
+
+// We keep everything private and only expose the instance.
+const muvizInstance = new Muviz();
+export default muvizInstance;
---
+++ src/main/tools/muviz/visualizers.ts
@@ -0,0 +1,236 @@
+import {CanvasController} from "../../../atoms/canvas";
+import {DropDetector} from "../../../common/audio";
+import {adjustColor} from "../../../common/color-utils";
+
+export enum VisualizerType {
+    Bar = 0,
+    Spiral = 1
+}
+
+export function toString(type: VisualizerType): string {
+    switch (type) {
+        case VisualizerType.Bar:
+            return "Bar";
+        case VisualizerType.Spiral:
+            return "Spiral";
+    }
+}
+
+interface VisualizerProps {
+    analyser: AnalyserNode;
+    dataArray: Uint8Array;
+}
+
+export class BaseVisualizer extends CanvasController {
+    protected readonly analyser: AnalyserNode;
+    protected readonly dataArray: Uint8Array;
+    
+    constructor({analyser, dataArray}: VisualizerProps) {
+        super();
+        this.analyser = analyser;
+        this.dataArray = dataArray;
+    }
+}
+
+export class BarVisualizer extends BaseVisualizer {
+    
+    constructor({analyser, dataArray}: VisualizerProps) {
+        super({analyser, dataArray});
+    }
+    
+    draw() {
+        
+        if (!this.canvasRef) {
+            return;
+        }
+        if (!this.canvasRef.current || !this.analyser || !this.dataArray) {
+            return;
+        }
+        
+        const canvas = this.canvasRef.current;
+        const ctx = canvas.getContext("2d");
+        
+        // Ensure the context is not null
+        if (!ctx) {
+            console.error("2D context could not be retrieved from canvas.");
+            return;
+        }
+        
+        const {width, height} = canvas;
+        ctx.clearRect(0, 0, width, height);
+        
+        this.analyser.getByteFrequencyData(this.dataArray);
+        
+        const barWidth = width / this.dataArray.length;
+        this.dataArray.forEach((value, index) => {
+            const barHeight = (
+                                  value / 255
+                              ) * height;
+            ctx.fillStyle = `rgb(${value}, 50, 200)`;
+            ctx.fillRect(index * barWidth, height - barHeight, barWidth, barHeight);
+        });
+    }
+}
+
+export class SpiralVisualizer extends BaseVisualizer {
+    private angle: number;
+    private points: any[];
+    private totalPoints: number;
+    private readonly growthRate: number;
+    private readonly maxGlow: number;
+    private canvasWidth: number;
+    private canvasHeight: number;
+    private dropDetector: DropDetector;
+    private readonly baseColor: string;
+    
+    constructor({analyser, dataArray}: VisualizerProps) {
+        super({analyser, dataArray});
+        
+        // Spiral properties
+        this.angle = 0; // Rotation angle of the spiral
+        this.points = []; // Active points in the spiral
+        this.totalPoints = 0; // Number of points added so far
+        
+        this.growthRate = 8; // Distance between consecutive points
+        this.maxGlow = 69; // Maximum glow intensity
+        
+        this.canvasWidth = 0; // Cached canvas width
+        this.canvasHeight = 0; // Cached canvas height
+        
+        this.baseColor = `rgb(200, 50, 150)`;
+        
+        this.dropDetector = new DropDetector({sampleRate: 44000});
+    }
+    
+    init() {
+        // Cache canvas dimensions
+        if (!this.canvasRef) {
+            return;
+        }
+        
+        const canvas = this.canvasRef.current;
+        if (canvas) {
+            this.canvasWidth = canvas.width;
+            this.canvasHeight = canvas.height;
+        }
+        console.log("SpiralVisualizer initialized.");
+    }
+    
+    addPoint() {
+        const angle = this.totalPoints * 0.5; // Spiral angle
+                                              // increment
+        const distance = this.totalPoints * this.growthRate; // Distance
+                                                             // from
+                                                             // center
+        
+        // Add a new point to the spiral
+        this.points.push({
+            x: distance * Math.cos(angle),
+            y: distance * Math.sin(angle),
+            size: 0.5, // Initial visual size of the point
+            angle: angle, // Angle of the point in the spiral
+        });
+        
+        this.totalPoints++;
+    }
+    
+    updatePoints(randomFactor = 0.0) {
+        const w = 1 - randomFactor;
+        // Update each point's position and size
+        this.points.forEach((point) => {
+            point.size *= 1.0069; // Gradual size increase
+            point.x = (
+                          w + randomFactor * Math.random()
+                      ) * 50 * point.size * Math.cos(point.angle); // Update
+                                                                   // x
+                                                                   // position
+            point.y = (
+                          w + randomFactor * Math.random()
+                      ) * 50 * point.size * Math.sin(point.angle); // Update
+                                                                   // y
+                                                                   // position
+        });
+        
+        // Remove points that move out of bounds
+        this.points = this.points.filter(
+            (point) =>
+                Math.abs(point.x) < this.canvasWidth && Math.abs(point.y) < this.canvasHeight
+        );
+    }
+    
+    drawPoints(ctx: CanvasRenderingContext2D | null) {
+        if (!ctx) {
+            return;
+        }
+        // Fetch audio data
+        this.analyser.getByteFrequencyData(this.dataArray);
+        
+        this.points.forEach((point, index) => {
+            // Map audio data to intensity
+            const soundLevel = this.dataArray[index % this.dataArray.length] / 255;
+            const intensity = soundLevel ** 2;
+            
+            // Set point properties based on audio intensity
+            const glow = intensity * this.maxGlow;
+            const brightnessVector = intensity > 0.9 ? [100, 100, 100] : [intensity, 1, 1 + 0.67 * intensity];
+            const color = adjustColor(this.baseColor, intensity + 0.5, brightnessVector);
+            
+            // Draw the point
+            ctx.beginPath();
+            ctx.arc(point.x, point.y, point.size * (
+                intensity > 0.8 ? 2 : 1
+            ), 0, Math.PI * 2);
+            
+            ctx.shadowBlur = glow;
+            ctx.shadowColor = color;
+            ctx.fillStyle = color;
+            ctx.fill();
+        });
+    }
+    
+    draw() {
+        if (!this.canvasRef) {
+            return;
+        }
+        
+        const canvas = this.canvasRef.current;
+        if (!canvas || !this.analyser || !this.dataArray) {
+            return;
+        }
+        const ctx = canvas.getContext("2d");
+        if (!ctx) {
+            return;
+        }
+        
+        ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight); // Clear
+                                                                  // the
+                                                                  // canvas
+        ctx.save();
+        ctx.translate(this.canvasWidth / 2, this.canvasHeight / 2); // Move
+                                                                    // the
+                                                                    // origin
+                                                                    // to
+                                                                    // the
+                                                                    // center
+                                                                    // of
+                                                                    // the
+                                                                    // canvas
+        ctx.rotate(this.angle); // Apply rotation
+        
+        const isDrop = this.dropDetector.detect(this.dataArray);
+        console.log("isDrop: ", isDrop);
+        
+        // Update and draw points
+        this.updatePoints(isDrop ? 0.05 : 0.0);
+        this.addPoint();
+        this.drawPoints(ctx);
+        ctx.restore();
+        
+        // Increment rotation angle for smooth animation
+        this.angle += (
+                          isDrop ? 2 : 1
+                      ) * 0.005;
+    }
+}
+
+
