cf3de92162c06a0d0768d280446cdf7b52056c61
Manoj Malviya <malviyamanoj1896@gmail.com>
2024-12-11 15:28:43 +0100

    adds makeLoop to make it static canvas

--- src/atoms/atom-canvas.tsx
+++ src/atoms/atom-canvas.tsx
@@ -3,10 +3,12 @@ import React, {useRef, useEffect, useState} from "react";
 export class AtomCanvasController {
     canvasRef: React.RefObject<HTMLCanvasElement> | React.RefObject<null> | undefined;
     animationFrameId: number | null;
+    makeLoop: boolean = true;
     
     constructor() {
         this.canvasRef = React.createRef();
         this.animationFrameId = null;
+        this.makeLoop = false;
     }
     
     setCanvasRef(canvasRef: React.RefObject<HTMLCanvasElement> | React.RefObject<null>) {
@@ -27,14 +29,14 @@ export class AtomCanvasController {
         // resources)
     }
     
-    start(makeLoop:boolean=true): void {
+    start(): void {
         const drawLoop = () => {
             this.draw();
             this.animationFrameId = requestAnimationFrame(drawLoop);
         };
         this.init();
         
-        if (makeLoop) {
+        if (this.makeLoop) {
             drawLoop();
         } else {
             this.draw();
@@ -47,6 +49,11 @@ export class AtomCanvasController {
         }
         this.cleanup();
     }
+    
+    restart(): void {
+        this.stop();
+        this.start();
+    }
 }
 
 
@@ -57,20 +64,23 @@ interface AtomCanvasProps {
 }
 
 export const AtomCanvas: React.FC<AtomCanvasProps> = ({
-                                                  controller = null,
-                                                    animationLoop = true,
-                                                  className = ""
-                                              }) => {
+                                                          controller = null,
+                                                          animationLoop = true,
+                                                          className = ""
+                                                      }) => {
     const canvasRef = useRef<HTMLCanvasElement | null>(null);
     
     useEffect(() => {
         if (!controller) {
+            if (canvasRef && canvasRef.current) {
+                canvasRef.current.textContent = "Canvas, Not available";
+            }
             return;
         }
         
         // Assign canvasRef to controller and start it
         controller.setCanvasRef(canvasRef as React.RefObject<HTMLCanvasElement>);
-        controller.start(animationLoop);
+        controller.makeLoop = animationLoop;
         
         // Cleanup on unmount
         return () => {
@@ -83,17 +93,23 @@ export const AtomCanvas: React.FC<AtomCanvasProps> = ({
         height: window.innerHeight,
     });
     
+    const restartController = () => {
+        if (controller) {
+            controller.restart();
+        }
+    }
+    
     useEffect(() => {
         const handleResize = () => {
             setDimensions({
                 width: window.innerWidth,
                 height: window.innerHeight,
             });
+            restartController();
         };
-        
         window.addEventListener("resize", handleResize);
         return () => window.removeEventListener("resize", handleResize);
-    }, []);
+    }, [setDimensions, restartController]);
     
     return (
         <canvas
