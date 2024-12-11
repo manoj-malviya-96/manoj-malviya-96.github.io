fb02557b523e4ba48f4243fed716d3e4788ff1b4
Manoj Malviya <malviyamanoj1896@gmail.com>
2024-12-11 01:56:09 +0100

    adds simple mesh via canvas

---
+++ src/main/tools/trussopt/truss-controller.ts
@@ -0,0 +1,88 @@
+import {useEffect, useState} from "react";
+import TrussMesh, {LatticeType} from "./truss-mesh";
+import {epsilon} from "numeric";
+import {AtomCanvasController} from "../../../atoms/atom-canvas";
+
+
+export class TrussStructureView extends AtomCanvasController {
+    private readonly mesh: TrussMesh;
+    
+    constructor(mesh: TrussMesh) {
+        super();
+        this.mesh = mesh;
+    }
+    
+    draw() {
+        if (!this.canvasRef) {
+            return;
+        }
+        if (!this.canvasRef.current || !this.mesh) {
+            return;
+        }
+        
+        const canvas = this.canvasRef.current;
+        const ctx = canvas.getContext("2d");
+        
+        // Ensure the context is not null
+        if (!ctx) {
+            console.error("2D context could not be retrieved from canvas.");
+            return;
+        }
+        let points = this.mesh.points;
+        const edges = this.mesh.connections;
+        
+        const {width, height} = canvas;
+        ctx.clearRect(0, 0, width, height);
+        const factor = Math.min(width / this.mesh.meshWidth, height / this.mesh.meshHeight);
+        
+        points = points.map(([x, y]) => [factor * x, factor * y]);
+        edges.forEach(([start, end]) => {
+            const [x1, y1] = points[start];
+            const [x2, y2] = points[end];
+            ctx.beginPath();
+            ctx.moveTo(x1, y1);
+            ctx.lineTo(x2, y2);
+            ctx.strokeStyle = "white";
+            ctx.stroke();
+            ctx.closePath();
+        });
+        
+        points.forEach(([x, y]) => {
+            ctx.beginPath();
+            ctx.arc(x, y, 6, 0, 2 * Math.PI);
+            ctx.fillStyle = "red";
+            ctx.fill();
+            ctx.closePath();
+        });
+        
+        
+    }
+}
+
+export const useTrussOpt = () => {
+    // Defaults
+    const [meshWidth, setMeshWidth] = useState<number>(60);
+    const [meshHeight, setMeshHeight] = useState<number>(20);
+    const [cellSize, setCellSize] = useState<number>(10);
+    const [latticeType, setLatticeType] = useState<LatticeType>(LatticeType.Cross);
+    const [controller, setController] = useState<AtomCanvasController | null>(null);
+    
+    
+    useEffect(() => {
+        const mesh = new TrussMesh({
+            meshWidth: meshWidth,
+            meshHeight: meshHeight,
+            cellSize: cellSize,
+            latticeType: latticeType,
+        });
+        setController(new TrussStructureView(mesh));
+    }, [meshWidth, meshHeight, cellSize, latticeType]);
+    
+    return {
+        meshWidth, setMeshWidth,
+        meshHeight, setMeshHeight,
+        cellSize, setCellSize,
+        latticeType, setLatticeType,
+        controller,
+    }
+}
--- src/main/tools/trussopt/trussopt.tsx
+++ src/main/tools/trussopt/trussopt.tsx
@@ -6,91 +6,103 @@ import AtomKnob from "../../../atoms/atom-knob";
 import AtomToggleButton from "../../../atoms/atom-toggle-button";
 import {AtomButton} from "../../../atoms/atom-button";
 import AtomDropdown from "../../../atoms/atom-dropdown";
+import {useTrussOpt} from "./truss-controller";
+import {LatticeType} from "./truss-mesh";
+import {AtomCanvas} from "../../../atoms/atom-canvas";
 
 const AppName = 'TrussOpt';
 
 const TrussOptView = () => {
-    
+    const {
+        meshWidth, setMeshWidth,
+        meshHeight, setMeshHeight,
+        cellSize, setCellSize,
+        setLatticeType, controller
+    } = useTrussOpt();
     
     return (
         <AppView
             appName={AppName}
             appLogo={Logo}
             children={
-                <div
-                    className=" absolute bottom-0 left-0 w-full h-fit
+                <div className="h-full w-full justify-center align-center">
+                    <AtomCanvas controller={controller} className="absolute
+                                                top-0 left-0 w-full h-2/3 translate-y-1/4 z-0 p-8"/>
+                    <div
+                        className=" absolute bottom-0 left-0 w-full h-fit
                         flex flex-row gap-4 rounded-lg
-                        border border-primary
+                        border border-primary z-5
                         justify-center items-center">
-                    <AtomKnob
-                        label='Cell Size (mm)'
-                        min={2}
-                        max={100}
-                        step={1}
-                        initValue={10}
-                        onChange={(e) => console.log(e)}
-                    />
-                    <AtomKnob
-                        label='Mesh Width (mm)'
-                        min={2}
-                        max={100}
-                        step={1}
-                        initValue={10}
-                        onChange={(e) => console.log(e)}
-                    />
-                    <AtomKnob
-                        label='Mesh Height (mm)'
-                        min={2}
-                        max={100}
-                        step={1}
-                        initValue={10}
-                        onChange={(e) => console.log(e)}
-                    />
-                    <AtomDropdown
-                        placeholder='Select Lattice Type'
-                        initialIndex={0}
-                        options={[
-                            {label: 'Cross', value: 'cross'},
-                            {
-                                label: 'Checkerboard',
-                                value: 'checkerboard'
-                            }
-                        ]}
-                        onClick={(e) => console.log(e)}
-                    />
-                    <AtomToggleButton
-                        offLabel='Fix Nodes'
-                        offIcon='pi pi-lock-open'
-                        onIcon='pi pi-lock'
-                        tooltip='Add fix nodes to the truss'
-                        initValue={false}
-                        onChange={(e) => console.log(e)}
-                    />
-                    <AtomToggleButton
-                        offLabel='Load Nodes'
-                        offIcon='pi pi-arrow-up'
-                        onIcon='pi pi-arrow-down'
-                        tooltip='Add Load nodes to the truss'
-                        initValue={false}
-                        onChange={(e) => console.log(e)}
-                    />
-                    <AtomButton
-                        label='Analyze'
-                        icon='pi pi-play'
-                        onClick={() => console.log('Analyze')}
-                    />
-                    <AtomButton
-                        label='Optimize'
-                        icon='pi pi-cog'
-                        severity={'info'}
-                        onClick={() => console.log('Optimize')}
-                    />
-                    <AtomButton
-                        label='Clear'
-                        icon='fas fa-trash'
-                        severity={'danger'}
-                        onClick={() => console.log('Refresh')}
-                    />
+                        <AtomKnob
+                            label='Cell Size (mm)'
+                            min={5}
+                            max={20}
+                            step={5}
+                            initValue={cellSize}
+                            onChange={setCellSize}
+                        />
+                        <AtomKnob
+                            label='Mesh Width (mm)'
+                            min={cellSize}
+                            max={100}
+                            step={cellSize}
+                            initValue={meshWidth}
+                            onChange={setMeshWidth}
+                        />
+                        <AtomKnob
+                            label='Mesh Height (mm)'
+                            min={cellSize}
+                            max={100}
+                            step={cellSize}
+                            initValue={meshHeight}
+                            onChange={setMeshHeight}
+                        />
+                        <AtomDropdown
+                            placeholder='Select Lattice Type'
+                            initialIndex={0}
+                            options={[
+                                {label: 'Cross', value: LatticeType.Cross},
+                                {
+                                    label: 'Checkerboard',
+                                    value: LatticeType.Checkerboard
+                                }
+                            ]}
+                            onClick={setLatticeType}
+                        />
+                        <AtomToggleButton
+                            offLabel='Fix Nodes'
+                            offIcon='pi pi-lock-open'
+                            onIcon='pi pi-lock'
+                            tooltip='Add fix nodes to the truss'
+                            initValue={false}
+                            onChange={(e) => console.log(e)}
+                        />
+                        <AtomToggleButton
+                            offLabel='Load Nodes'
+                            offIcon='pi pi-arrow-up'
+                            onIcon='pi pi-arrow-down'
+                            tooltip='Add Load nodes to the truss'
+                            initValue={false}
+                            onChange={(e) => console.log(e)}
+                        />
+                        <AtomButton
+                            label='Analyze'
+                            icon='pi pi-play'
+                            onClick={() => console.log('Analyze')}
+                        />
+                        <AtomButton
+                            label='Optimize'
+                            icon='pi pi-cog'
+                            severity={'info'}
+                            onClick={() => console.log('Optimize')}
+                        />
+                        <AtomButton
+                            label='Clear'
+                            icon='fas fa-trash'
+                            severity={'danger'}
+                            onClick={() => console.log('Refresh')}
+                        />
+                    </div>
                 </div>
             }
         />
