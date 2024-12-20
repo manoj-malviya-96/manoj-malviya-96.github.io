12f298e3421066778e88d423fc083e505e703000
Manoj Malviya <malviyamanoj1896@gmail.com>
2024-12-20 20:07:50 +0100

    init analytics

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import RouterConstructor from "./common/router";
 import constructedRoutes from "./main/routes";
 import {ScreenSizeProvider} from "./common/screen";
 import {ThemeProvider} from "./common/theme";
+import {init as initAnalytics} from "./main/analytics";
 
 const App = () => {
+    initAnalytics();
     return (
         <ThemeProvider>
             <ScreenSizeProvider>
---
+++ src/main/analytics.ts
@@ -0,0 +1,9 @@
+import TagManager from 'react-gtm-module'
+
+const tagManagerArgs = {
+    gtmId: 'G-T2JX2G6X81'
+}
+
+export function init() {
+    TagManager.initialize(tagManagerArgs)
+}
