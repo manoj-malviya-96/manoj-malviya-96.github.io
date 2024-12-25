443ad2d30a2ca7b4a31215b97fef0b4805374a70
Manoj Malviya <malviyamanoj1896@gmail.com>
2024-12-25 16:49:08 +0100

    photo has overlay

--- src/atoms/atom-image.tsx
+++ src/atoms/atom-image.tsx
@@ -34,14 +34,19 @@ const AtomImage: React.FC<AtomImageProps> = ({
     return (
         <>
             {/* Normal Image */}
-            <div className={`relative ${className} overflow-clip`}>
+            <div className={`relative ${className} overflow-clip items-center justify-center`}>
                 <img
                     src={src}
                     alt={alt}
                     loading="lazy"
-                    className={`cursor-pointer ${preview ? "hover:opacity-90" : ""}`}
-                    onClick={preview ? toggleFullScreen : undefined}
                 />
+                <div className={`absolute inset-0 bg-primary
+                                flex items-center justify-center cursor-pointer
+                                opacity-0 ${preview ? "hover:bg-opacity-50 hover:opacity-90" : ""} `}
+                     onClick={preview ? toggleFullScreen : undefined}
+                >
+                    <i className={"fas fa-magnifying-glass text-2xl"}/>
+                </div>
                 {showLabel && <span className="text-sm text-accent">{alt}</span>}
             </div>
             
