d47f9d40bc246baaf149b7598ed1a2f154d2eeef
Manoj Malviya <malviyamanoj1896@gmail.com>
2025-01-08 13:56:24 +0100

    fallback option

--- src/providers/firebase.tsx
+++ src/providers/firebase.tsx
@@ -12,15 +12,23 @@ type FirebaseContextType = {
 const FirebaseContext = createContext<FirebaseContextType | undefined>(undefined);
 
 export const FirebaseProvider: React.FC<{ children: React.ReactNode }> = ({children}) => {
+	
 	const firebaseConfig = {
-		apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-		authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-		projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-		storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-		messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-		appId: process.env.REACT_APP_FIREBASE_APP_ID,
-		measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+		apiKey: process.env.REACT_APP_FIREBASE_API_KEY || '',
+		authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || '',
+		projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || '',
+		storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || '',
+		messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID || '',
+		appId: process.env.REACT_APP_FIREBASE_APP_ID || '',
+		measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID || '',
 	};
+	
+	// Check for missing values and log them
+	Object.entries(firebaseConfig).forEach(([key, value]) => {
+		if (!value) {
+			console.error(`Firebase configuration missing: ${key}`);
+		}
+	});
 	
 	
 	const app: FirebaseApp = initializeApp(firebaseConfig);
