b4c0ecdd74f62a562946a88e18c7726160248911
Manoj Malviya <malviyamanoj1896@gmail.com>
2025-01-08 13:43:41 +0100

    adds firebase

--- src/providers/firebase.tsx
+++ src/providers/firebase.tsx
@@ -1,28 +1,31 @@
 import {initializeApp} from "firebase/app";
 import {getAnalytics} from "firebase/analytics";
 import React, {createContext, useContext} from "react";
-
-const firebaseConfig = {
-	apiKey: process.env.REACT_APP_FIREBASE_API_KEY || '',
-	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || '',
-	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || '',
-	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || '',
-	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID || '',
-	appId: process.env.REACT_APP_FIREBASE_APP_ID || '',
-	measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID || '',
-};
-
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+import {FirebaseApp} from "@firebase/app";
+import {Analytics} from "@firebase/analytics"
 
 type FirebaseContextType = {
-	app: typeof app;
-	analytics?: typeof analytics;
+	app: FirebaseApp;
+	analytics?: Analytics;
 };
 
 const FirebaseContext = createContext<FirebaseContextType | undefined>(undefined);
 
 export const FirebaseProvider: React.FC<{ children: React.ReactNode }> = ({children}) => {
+	const firebaseConfig = {
+		apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+		authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+		projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+		storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+		messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+		appId: process.env.REACT_APP_FIREBASE_APP_ID,
+		measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+	};
+	
+	
+	const app: FirebaseApp = initializeApp(firebaseConfig);
+	const analytics = getAnalytics(app);
+	
 	return (
 		<FirebaseContext.Provider value={{app, analytics}}>
 			{children}
