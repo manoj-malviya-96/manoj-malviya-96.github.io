c546844bd26bdcfc570989812b43616283e26ced
Manoj Malviya <malviyamanoj1896@gmail.com>
2025-01-18 09:25:01 +0100

    remove unused

--- src/main/all-providers.tsx
+++ src/main/all-providers.tsx
@@ -4,23 +4,20 @@ import KeyboardShortcutProvider from "../providers/keyboard";
 import {NavbarProvider} from "../providers/navbar";
 import {ThemeProvider} from "../providers/theme";
 import ToastProvider from "../providers/toasts";
-import {FirebaseProvider} from "../providers/firebase";
 
 const AllProviders: React.FC<{ children: React.ReactNode }> = ({children}) => {
 	return (
-		<FirebaseProvider>
-			<ThemeProvider>
-				<ScreenSizeProvider>
-					<KeyboardShortcutProvider>
-						<NavbarProvider>
-							<ToastProvider>
-								{children}
-							</ToastProvider>
-						</NavbarProvider>
-					</KeyboardShortcutProvider>
-				</ScreenSizeProvider>
-			</ThemeProvider>
-		</FirebaseProvider>
+		<ThemeProvider>
+			<ScreenSizeProvider>
+				<KeyboardShortcutProvider>
+					<NavbarProvider>
+						<ToastProvider>
+							{children}
+						</ToastProvider>
+					</NavbarProvider>
+				</KeyboardShortcutProvider>
+			</ScreenSizeProvider>
+		</ThemeProvider>
 	);
 };
 export default AllProviders;
