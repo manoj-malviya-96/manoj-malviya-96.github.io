37a6065d7858ea49e59bc35943c85ac957f624c2
Manoj Malviya <malviyamanoj1896@gmail.com>
2025-01-23 08:51:53 +0100

    replaces with smaller image

--- src/main/hobbies.tsx
+++ src/main/hobbies.tsx
@@ -6,16 +6,17 @@ import {
   LayoutSize,
 } from "../atoms/atom-layout";
 import AtomImage from "../atoms/atom-image";
-import Sweden from "./assets/photography/sweden.jpg";
-import Warsaw1 from "./assets/photography/warsaw-1.jpg";
-import Warsaw2 from "./assets/photography/warsaw-2.jpg";
-import JustMe from "./assets/photography/manoj.jpg";
-import Running from "./assets/photography/running.jpg";
-import Northern from "./assets/photography/northern.jpg";
-import Denali from "./assets/photography/denali.jpg";
-import Alka from "./assets/photography/alka.jpg";
 
-import React from "react";
+import Sweden from "./assets/photography/sweden-min.jpg";
+import Warsaw1 from "./assets/photography/warsaw-1-min.jpg";
+import Warsaw2 from "./assets/photography/warsaw-2-min.jpg";
+import JustMe from "./assets/photography/manoj-min.jpg";
+import Running from "./assets/photography/running-min.jpg";
+import Northern from "./assets/photography/northern-min.jpg";
+import Denali from "./assets/photography/denali-min.jpg";
+import Alka from "./assets/photography/alka-min.jpg";
+
+import React, { useState } from "react";
 import { AtomHeroBrandTitleText, AtomHeroTitleText } from "../atoms/atom-text";
 import AtomStyledContainer from "../atoms/atom-styled-container";
 import YoutubeMusicPlayer from "../atoms/misc/youtube-music-player";
