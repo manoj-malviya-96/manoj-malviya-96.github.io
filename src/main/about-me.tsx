27670a37c8c81c822f0ab6ce3f6debd41b129533
Manoj Malviya <malviyamanoj1896@gmail.com>
2025-01-26 18:33:50 +0100

    adds overlaay

--- src/main/about-me.tsx
+++ src/main/about-me.tsx
@@ -67,7 +67,11 @@ const SocialMediaButtons = () => {
     },
   );
   return (
-    <AtomRow size={LayoutSize.FullWidth} alignment={LayoutAlign.Start}>
+    <AtomRow
+      size={LayoutSize.FullWidth}
+      alignment={LayoutAlign.Start}
+      gap={LayoutGap.None}
+    >
       {socialMediaItems.map((item, index) => (
         <AtomButton key={index} {...item} />
       ))}
@@ -89,20 +93,24 @@ const CareerHighlights = () => {
   return <AtomTimeline items={timelineData} className={"w-full h-full"} />;
 };
 
-const AboutMeText = () => {
+const WhoIsManoj = () => {
   return (
-    <AtomColumn gap={LayoutGap.Small} size={LayoutSize.None}>
+    <AtomColumn
+      className={"w-full md:w-1/2 h-full"}
+      size={LayoutSize.None}
+      gap={LayoutGap.None}
+    >
       <AtomImage src={ProfilePicture} alt={"Cover"} />
-      <AtomHeroBrandTitleText className={"w-full mt-8"}>
+      <AtomHeroBrandTitleText className={"w-full mt-4"}>
         Manoj Malviya
       </AtomHeroBrandTitleText>
       <AtomPrimaryText className={"w-fit p-0 justify-start"}>
         I’m a Software Engineer with over 6 years of experience in Physics
-        Simulation, CAD, and Software development (Desktop/Web).
-        <br /> <br />
-        I’m known for leading innovative projects with a product-focused
-        approach that aligns well with business goals.
+        Simulation, CAD, and Software development (Desktop/Web). I’m known for
+        leading innovative projects with a product-focused approach that aligns
+        well with business goals.
       </AtomPrimaryText>
+      <SocialMediaButtons />
     </AtomColumn>
   );
 };
@@ -116,10 +124,7 @@ const AboutMe = () => {
       smallDeviceAdjustment={true}
       className={"mt-4 h-full px-6"}
     >
-      <AtomColumn className={"w-full md:w-1/2 h-full"} size={LayoutSize.None}>
-        <AboutMeText />
-        <SocialMediaButtons />
-      </AtomColumn>
+      <WhoIsManoj />
       <AtomStyledContainer className={"w-full h-full"} transparency={true}>
         <AtomColumn size={LayoutSize.FullSize} gap={LayoutGap.Small}>
           <CareerHighlights />
