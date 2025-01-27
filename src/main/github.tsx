6cd46f2431a137a9a7a7b372d83e702c53f59ae7
Manoj Malviya <malviyamanoj1896@gmail.com>
2025-01-27 19:14:21 +0100

    adds new option of accumulation

--- src/main/github.tsx
+++ src/main/github.tsx
@@ -3,10 +3,11 @@ import {
   AtomColumn,
   AtomRow,
   LayoutAlign,
+  LayoutGap,
   LayoutSize,
 } from "../atoms/atom-layout";
 import AtomCalendarChart from "../atoms/charts/atom-calendar-chart";
-import { CurrentYear } from "../common/date";
+import { CurrentYear, getCurrentDayInYear } from "../common/date";
 import { AtomTitleText } from "../atoms/atom-text";
 import { AtomLoader } from "../atoms/atom-loader";
 import AtomStats, { Stats } from "../atoms/atom-stats";
@@ -22,9 +23,12 @@ import {
   TabBarOrientation,
   TabButtonProps,
 } from "../atoms/atom-bars";
+import AtomDropdown from "../atoms/atom-dropdown";
+import AtomRaceLineChart from "../atoms/charts/atom-race-line";
 
 const API_URL = "https://github-contributions-api.jogruber.de/v4/";
 const ChartHeight = 210;
+const GithubColor = "#00dc2c";
 
 export interface Activity {
   date: string;
@@ -70,23 +74,69 @@ export const fetchGithubData = async ({
   return data as GithubApiResponse;
 };
 
-function transformDataForEChart(
-  data: GithubApiResponse,
-): Record<string, number> {
+interface GithubPlotProps {
+  data: GithubApiResponse;
+  year: number;
+}
+
+const GithubRaceLinePlot: React.FC<GithubPlotProps> = ({ data, year }) => {
+  const maxLength =
+    year !== CurrentYear ? data.contributions.length : getCurrentDayInYear();
+  const allDates = aRange(0, maxLength, 1);
+  const accumulatedContributions = data.contributions
+    .reduce((acc, activity) => {
+      acc.push((acc[acc.length - 1] || 0) + activity.count);
+      return acc;
+    }, [] as number[])
+    .slice(0, maxLength);
+
+  const chartData = [
+    {
+      label: "Contributions",
+      data: accumulatedContributions,
+    },
+  ];
+  return (
+    <AtomRaceLineChart
+      data={chartData}
+      time={allDates}
+      height={ChartHeight}
+      xAxisLabel="Day of the year"
+      yAxisLabel="Contributions"
+      mainColor={GithubColor}
+    />
+  );
+};
+
+const GithubCalendarPlot: React.FC<GithubPlotProps> = ({ data, year }) => {
   const transformedData: Record<string, number> = {};
   data.contributions.forEach((activity) => {
     transformedData[activity.date] = activity.count;
   });
-  return transformedData;
+  return (
+    <AtomCalendarChart
+      data={transformedData}
+      year={year}
+      unit={"contributions"}
+      height={ChartHeight}
+      mainColor={GithubColor}
+    />
+  );
+};
+
+export enum GithubPlotType {
+  Calendar = 0,
+  Accumulated = 1,
 }
 
-export const GithubCalendar = () => {
+export const GithubView = () => {
   // const [user, setUser] = useState<string>('manoj-malviya-96');
   const user = "manoj-malviya-96";
   const [data, setData] = useState<GithubApiResponse | null>(null);
   const [error, setError] = useState<GithubApiErrorResponse | null>(null);
   const [year, setYear] = useState<Year>(CurrentYear - 1);
   const [loading, setLoading] = useState<boolean>(false);
+  const [type, setType] = useState<GithubPlotType>(GithubPlotType.Calendar);
 
   const [longestStreak, setLongestStreak] = useState<Stats>(undefined);
   const [total, setTotal] = useState<Stats>(undefined);
@@ -129,32 +179,56 @@ export const GithubCalendar = () => {
       }) as TabButtonProps,
   );
 
+  const _year = year === "last" ? CurrentYear - 1 : year;
+
   return (
     <div className={"w-full h-full inline-block"}>
       <AtomColumn size={LayoutSize.FullSize}>
         {data && !loading && (
           <AtomColumn size={LayoutSize.FullSize}>
-            <AtomCalendarChart
-              data={transformDataForEChart(data)}
-              year={year === "last" ? CurrentYear - 1 : year}
-              unit={"contributions"}
-              height={ChartHeight}
-            />
+            <>
+              {type === GithubPlotType.Calendar && (
+                <GithubCalendarPlot data={data} year={_year} />
+              )}
+              {type === GithubPlotType.Accumulated && (
+                <GithubRaceLinePlot data={data} year={_year} />
+              )}
+            </>
+            {/* Controls */}
             <AtomRow
               alignment={LayoutAlign.CenterBetween}
               size={LayoutSize.FullWidth}
               smallDeviceAdjustment
             >
-              <AtomRow size={LayoutSize.FullWidth}>
-                <AtomStats text={"Total Contributions"} value={total} />
-                <AtomStats text={"Longest Streak"} value={longestStreak} />
-                <AtomStats text={"Daily Average"} value={dailyAverage} />
-              </AtomRow>
+              <AtomDropdown
+                onClick={setType}
+                initialIndex={0}
+                className={"w-32"}
+                options={[
+                  {
+                    label: "Calendar",
+                    value: GithubPlotType.Calendar,
+                  },
+                  {
+                    label: "Accumulated",
+                    value: GithubPlotType.Accumulated,
+                  },
+                ]}
+              />
               <AtomButtonBar
                 className={"px-4 md:px-0 w-fit"}
                 items={buttonsProps}
                 orientation={TabBarOrientation.Horizontal}
               />
+              <AtomRow
+                gap={LayoutGap.Medium}
+                size={LayoutSize.None}
+                className={"w-fit md:w-1/2"}
+              >
+                <AtomStats text={"Total Contributions"} value={total} />
+                <AtomStats text={"Longest Streak"} value={longestStreak} />
+                <AtomStats text={"Daily Average"} value={dailyAverage} />
+              </AtomRow>
             </AtomRow>
           </AtomColumn>
         )}
