d909d0e336693eca58cb31d77816a6f88f581eb9
Manoj Malviya <malviyamanoj1896@gmail.com>
2025-01-30 00:11:23 +0100

    in view load

--- src/main/tools/mesha/mesha.tsx
+++ src/main/tools/mesha/mesha.tsx
@@ -4,15 +4,19 @@ import Logo from "../logos/mesha.svg";
 import AppView from "../app-view";
 import AtomThreeCanvas from "../../../atoms/three/atom-three-canvas";
 import { ExampleCubeMesh } from "../../../atoms/three/atom-three-mesh";
+import AtomInViewContainer from "../../../atoms/atom-in-view-container";
 
 const AppName = "MESHA";
 
 const MeshaView = () => {
+  const [show, setShow] = React.useState(false);
   const cubeMesh = new ExampleCubeMesh();
 
   return (
     <AppView appName={AppName} appLogo={Logo}>
-      <AtomThreeCanvas meshes={[cubeMesh]} />
+      <AtomInViewContainer onInView={() => setShow(true)}>
+        {show && <AtomThreeCanvas meshes={[cubeMesh]} />}
+      </AtomInViewContainer>
     </AppView>
   );
 };
